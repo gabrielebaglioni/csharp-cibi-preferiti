d09bef12f2f2d7310acc367d637ad299d12b1feb
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-09-15 15:51:32 +0200

    first commit

---
+++ index.cs
@@ -0,0 +1,21 @@
+//Console.WriteLine("Hello, World!");
+
+string[] food = { "pasta", "pizza", "patatine", "burger", "tonno" };
+Console.Write("numero di cibi ");
+Console.Write(food.Length);
+Console.WriteLine(" ");
+
+food.ToList().ForEach(Console.WriteLine);
+Console.WriteLine(" ");
+
+
+Console.WriteLine(" ");
+Console.Write("il mio cibo prefrito in assoluto è ");
+Console.Write(food[1]);
+Console.WriteLine(" ");
+Console.Write("Il mio cibo preferito ma non troppo è ");
+Console.Write(food[4]);
+
+Console.WriteLine(" ");
+Console.Write("Il mio cibo intermedio è ");
+Console.Write(food[2]);
