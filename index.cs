a8611b772860a23410827429c7269a60ca7afa31
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-09-16 09:24:52 +0200

    fix

--- index.cs
+++ index.cs
@@ -2,20 +2,16 @@
 
 string[] food = { "pasta", "pizza", "patatine", "burger", "tonno" };
 Console.Write("numero di cibi " + food.Length);
-//Console.Write(food.Length);
 Console.WriteLine(" ");
+
 
 food.ToList().ForEach(Console.WriteLine);
 Console.WriteLine(" ");
 
 
 Console.WriteLine(" ");
-Console.Write("il mio cibo prefrito in assoluto è ");
-Console.Write(food[1]);
+Console.Write("il mio cibo prefrito in assoluto è " + food[1]);
 Console.WriteLine(" ");
-Console.Write("Il mio cibo preferito ma non troppo è ");
-Console.Write(food[4]);
-
+Console.Write("Il mio cibo preferito ma non troppo è " + food[4]);
 Console.WriteLine(" ");
-Console.Write("Il mio cibo intermedio è ");
-Console.Write(food[food.Length/2]);
+Console.Write("Il mio cibo intermedio è " + food[food.Length / 2]);
